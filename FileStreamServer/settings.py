d2005f8ce73e0e7eab4044e6c51b9d529280a94a
rushabhmavkar <rushabhmavkar972000@gmail.com>
2022-02-17 22:37:28 +0530

    Added static root path.

--- FileStreamServer/settings.py
+++ FileStreamServer/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,6 +102,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
