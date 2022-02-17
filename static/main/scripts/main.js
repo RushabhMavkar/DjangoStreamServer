6d22a19bf576d295dd99d50fb92c51d744304d97
rushabhmavkar <rushabhmavkar972000@gmail.com>
2022-02-17 22:45:46 +0530

    Added collectstatic static files

---
+++ static/main/scripts/main.js
@@ -0,0 +1,39 @@
+var route_api_files = '../../api/files/'
+var route_api_view_file = '../../view/file/'
+
+var folder_svg_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16"><path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/></svg>'
+var eye_fill_svg_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/></svg>'
+var eye_slash_svg_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16"><path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/><path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/></svg>'
+
+
+function getIcon(type){
+    if(type == 'dir'){
+        return folder_svg_icon
+    }
+    else{
+        return type
+    }
+}
+
+function ShowHiddenText(){
+    var hidden_text = document.getElementById('server-join-code');
+    var hidden_text_icon = document.getElementById('text-hidden-icon');
+    console.log(hidden_text_icon.innerHtml);
+
+    hidden_text.classList.toggle("text-disc");
+
+    if(hidden_text.classList.contains("text-disc")){
+        hidden_text_icon.innerHTML = eye_fill_svg_icon;
+    }
+    else{
+        hidden_text_icon.innerHTML = eye_slash_svg_icon;
+    }
+}
+
+function openModel() {
+  document.getElementById("share-model").style.display = "block";
+}
+
+function closeModel() {
+  document.getElementById("share-model").style.display = "none";
+}
