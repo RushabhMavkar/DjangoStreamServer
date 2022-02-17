27f96788c1ccafa5e9053960859069c15296b184
rushabhmavkar <rushabhmavkar972000@gmail.com>
2022-02-17 22:17:49 +0530

    Added activated field in UserAccess model

--- main/models.py
+++ main/models.py
@@ -7,15 +7,17 @@ class Server(models.Model):
     name = models.CharField(max_length=256)
     is_secure = models.BooleanField(default=True)
     access = models.CharField(max_length=1024, default="C:\\GATE")
+    created_on = models.DateTimeField(auto_now_add=True)
 
 
 class UserBookmark(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    path = models.FilePathField()
+    path = models.CharField(max_length=1024)
 
 
 class UserAccess(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     server = models.ForeignKey(to=Server, on_delete=models.DO_NOTHING)
+    activated = models.IntegerField(default=0)
     has_validity = models.BooleanField(default=False)
     valid_till = models.DateTimeField()
