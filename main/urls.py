a47e65d0a17c6f5900916267dc275f732e6ba5cb
rushabhmavkar <rushabhmavkar972000@gmail.com>
2022-02-22 21:13:24 +0530

    Added routes to join/delete server and view video.

--- main/urls.py
+++ main/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
     path('api/files/', views.api_view_server_files, name='main_api_view_files'),
 
     path('server/create/', views.create_server, name='main_create_server'),
+    path('server/join/', views.join_server, name='main_join_server'),
+    path('server/delete/', views.delete_server, name='main_delete_server'),
+
     path('view/file/', views.serve_file, name='main_serve_file'),
+    path('view/video/', views.view_video, name='main_video_player'),
 
     path('qrcode/', views.share_qrcode, name='main_share_qrcode'),
 ]
