927de58fea9a68c6cb2c2f86563980e4862317f6
rushabhmavkar <rushabhmavkar972000@gmail.com>
2022-02-22 21:10:22 +0530

    Changed get_ip return format and added function to guess file type.

--- main/helper_functions.py
+++ main/helper_functions.py
@@ -1,13 +1,15 @@
+import os.path
+
 import pyqrcode
 import socket
-import hashlib
-import string
 import random
+import mimetypes
 
 
 def get_ip():
     hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
+    ip_info = socket.gethostbyname_ex(hostname)
+    ip_address = ip_info[-1][-1]
     return ip_address
 
 
@@ -25,3 +27,11 @@ def generate_hash(length):
     sample_string = 'demostring'
     x = ''.join((random.choice(sample_string)) for x in range(length))
     return x
+
+
+def guess_type(path):
+    info = mimetypes.guess_type(path)[0]
+    if info:
+        type = info.split('/')[0]
+        return type
+    return 'application'
